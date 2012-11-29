543352a3c0ad56930b6dff74897d9cc3c69f43c6
waynix <github@klappezu.org>
2012-11-29 11:52:01 +0100

    Update LEDkontrol.c
    Comments and code cleanup

--- LEDkontrol.c
+++ LEDkontrol.c
@@ -2,15 +2,20 @@
  * spitest.c
  *
  *  Created on: 07.07.2012
- *      Author: paul
+ *      Author: Paul Rosset
  */
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <stdio.h>
 #include <util/delay.h>
 #include "spi.h"
-
+/**
+ * Global Variables
+ */
 volatile int16_t framebuffer[]={0x0,0x0,0x0800,0x03F,0x00,0x00,0x00,0x0};
+int8_t serialize[4];
+volatile uint8_t counter;
+
 uint8_t getRed(int16_t c16)
 {
 	return (c16 >> 10)& 0x3E;
@@ -23,10 +28,12 @@ uint8_t getBlue(int16_t c16)
 {
 	return (c16 << 1) & 0x3E;
 }
+/** Set the LED array to a new Image*/
 void update(int h)
 {
 	for (int i = 0; i < 8; i++)
 	{
+		//HSV colorspace
 		int current = (h+i * 10)%360;//46
 		int8_t block = (current) / 60;
 		int16_t hi = block <<5;
@@ -40,7 +47,7 @@ void update(int h)
 		int t = f;
 		int V= 31;
 
-
+                //Set led to color value
 		switch (block)
 		{
 		case 0:
@@ -72,6 +79,9 @@ void update(int h)
 	}
 
 }
+/**
+ * Convert the Color value for each LED to power times
+ */
 void renderColor(volatile int16_t buff[], unsigned int counter, int ledcount)
 {
 	int8_t tmp = 0;
@@ -102,9 +112,13 @@ void renderColor(volatile int16_t buff[], unsigned int counter, int ledcount)
 		}
 	}
 }
-int8_t serialize[4];
+
+/**
+ * Initialisation routine
+ * */
 int main()
 {
+	//Set the timer
 	//WGM10 - 13 ctc
 	TCCR1A = (0 << WGM10) | (0 << WGM11);
 	TCCR1B = (0 << WGM13) | (1 << WGM12);
@@ -117,6 +131,7 @@ int main()
 	int16_t cnt = 0;
 	DDRD |= (1 << PD5);
 	sei();
+	//Main loop
 	while(1)
 	{
 		_delay_ms(50);
@@ -133,7 +148,11 @@ int main()
 }
 
 ISR(TIMER1_OVF_vect){}
-volatile uint8_t counter;
+
+/**
+ * Timer1 Overflow Vector
+ * This timer is used to switch the leds on and of depending on their color value
+ * */
 ISR(TIMER1_COMPA_vect)
 {
     char tmp = SREG; //backup SREG state
@@ -141,10 +160,10 @@ ISR(TIMER1_COMPA_vect)
 
     PORTB &= ~(1<<(SPI_SlaveSelect));
 
-
     counter++;
     counter %= 0x3F;
     renderColor(framebuffer,counter,8);
+    
     PORTB |= (1<<SPI_SlaveSelect);
     SREG = tmp;//Restore SREG
 }
